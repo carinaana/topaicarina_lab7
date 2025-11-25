b79ce44d9bec7536c04585cba35bd6aa72abc2bb
Topai Carina Ana <topai.carina@yahoo.ro>
2025-11-25 12:10:06 +0200

    push lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace topaicarina_lab7
+﻿using System;
+using topaicarina_lab7.Data;
+using System.IO;
+
+namespace topaicarina_lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
