2299a9788c8260a7c08e86c4f5b6b5b6937e7823
Topai Carina Ana <topai.carina@yahoo.ro>
2025-12-01 01:26:35 +0200

    laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -23,4 +23,40 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+
+        await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        if (listView.SelectedItem != null)
+        {
+            var selectedProduct = listView.SelectedItem as Product;
+            var shopList = (ShopList)BindingContext;
+
+            await App.Database.DeleteListProductAsync(shopList.ID, selectedProduct.ID);
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+            listView.SelectedItem = null;
+        }
+        else
+        {
+            await DisplayAlert("Eroare", "Selecteaza un produs pentru a-l sterge.", "OK");
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
 }
